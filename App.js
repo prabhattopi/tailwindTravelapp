ba5e1e420347d56aabcadacefd5d52270e17a1f9
prabhattopi <topi9864@gmail.com>
2023-06-27 14:41:00 +0530

    the tailwind config is done

---
+++ App.js
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { TailwindProvider } from 'tailwindcss-react-native';
+
+export default function App() {
+  return (
+    <TailwindProvider>
+      <SafeAreaView className="flex-1 items-center justify-center bg-blue-500 ">
+        <Text className="text-white">
+          Open up App.js to start working on your app!
+        </Text>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </TailwindProvider>
+  )
+}
